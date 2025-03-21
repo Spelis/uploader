a302433c6085c5e36717349d811c556c682a4339
Elis Eriksson <Spelis@duck.com>
2025-03-21 08:39:53 +0100

    baller

---
+++ db.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timezone
+from typing import Annotated, AsyncGenerator
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import Field, SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+engine = create_async_engine("sqlite+aiosqlite:///uploader.db", echo=True, future=True)
+
+
+# This is now handled using alembic migrations
+# async def init_db():
+#     async with engine.begin() as conn:
+#         # await conn.run_sync(SQLModel.metadata.drop_all)
+#         await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    async with async_session() as session:
+        yield session
+
+
+class UserCredentials(SQLModel):
+    username: str = Field(unique=True)
+    password: str
+
+
+class User(UserCredentials, table=True):
+    id: int = Field(default=None, primary_key=True)
+
+
+class File(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    filename: str
+    owner_id: int = Field(foreign_key="user.id")
+    uploaded_at: int = Field(
+        default_factory=lambda: datetime.now(timezone.utc).timestamp()
+    )
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
---
+++ main.py
@@ -0,0 +1,132 @@
+import os
+from datetime import datetime, timezone
+from typing import Annotated, Any
+from dotenv import load_dotenv
+from fastapi import Depends, Response
+from fastapi import FastAPI as FA
+from fastapi import HTTPException, Security, Header
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from jose.exceptions import JWTError
+from jose.jwt import decode, encode
+from pydantic import BaseModel
+from sqlmodel import select
+import random
+from PIL import Image
+import base64
+
+from db import SessionDep, User, UserCredentials
+
+security = HTTPBearer()
+
+CredentialsDependency = Annotated[HTTPAuthorizationCredentials, Depends(security)]
+
+jwt_secret = "secret"  # Load this from env or some shit
+
+
+def generate_jwt_token(user: User) -> str:
+    payload = {
+        "sub": user.username,
+        "iat": datetime.now(timezone.utc).timestamp(),
+    }
+    return encode(payload, jwt_secret, algorithm="HS256")
+
+
+async def get_user_session(db: SessionDep, credentials: CredentialsDependency) -> User:
+    token = credentials.credentials
+    try:
+        payload = decode(token, jwt_secret, algorithms=["HS256"])
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user_name = payload["sub"]
+    user = (await db.exec(select(User).where(User.username == user_name))).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return user
+
+def get_filepath(id:int):
+    os.makedirs(f"files/{id}/", exist_ok=True)
+    return f"files/{id}/{round(datetime.now().timestamp()*1000)}"
+
+def get_existing_filepath(id:int,ts:int):
+    os.makedirs(f"files/{id}/", exist_ok=True) # avoid errors
+    return f"files/{id}/{ts}"
+
+
+UserDependency = Annotated[User, Depends(get_user_session)]
+
+
+app = FA()
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
+
+
+@app.post("/me")  # requires auth
+async def user_info(user: UserDependency):
+    #return user
+    return {"passwd": user.password, "username": user.username, "ID": user.id, "files": os.listdir(f"files/{user.id}/"), "help":[
+        "POST /login",
+        "POST /register",
+        "POST /me",
+        "POST /up/{fmt}",
+        "GET  /files/{id}/{file}"
+    ]}
+
+
+@app.post("/login")
+async def signin(credentials: UserCredentials, session: SessionDep):
+    # PK is id, but we have username
+    user = (
+        await session.exec(select(User).where(User.username == credentials.username))
+    ).first()
+    if user is None:
+        return {"error": "Invalid credentials"}
+    if user.password != credentials.password:
+        return {"error": "Invalid credentials"}
+    return {"token": generate_jwt_token(user)}
+
+
+@app.post("/register")
+async def register(credentials: UserCredentials, session: SessionDep):
+    user = User.model_validate(credentials)
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return {"token": generate_jwt_token(user)}
+
+@app.post("/up/{fmt}")
+async def upload(credentials: UserDependency,data: Annotated[str | bytes, Header()],fmt:str):
+    fp = get_filepath(credentials.id)
+    if fmt == "img":
+        with open(fp + ".png", "wb") as f:
+            f.write(base64.b64decode(data))
+        return {"success": "Image saved successfully", "saved_to": fp + ".png"}
+    if fmt == "txt":
+        with open(fp,"w") as f:
+            f.write(data)
+        return {"success": "Text saved successfully", "saved_to": fp}
+    else:
+        return {"error": "Invalid format; Available formats: img, txt"}
+    
+@app.get("/files/{id}/{file}")
+async def get_file(id, file:str):
+    with open(get_existing_filepath(id,file),'rb') as f:
+        content = f.read()
+        if file.endswith('.png'):
+            return Response(content,media_type="image/png")
+        else:
+            return content.decode('utf-8')
+        return content
+"""
+HOW TO USE ALEMBIC:
+
+TO CREATE A NEW TABLE:
+    alembic revision --autogenerate -m "update_name"
+
+THEN TO GO migrations/versions/someid_update_name.py AND ADD AN IMPORT FOR SQLMODEL
+
+TO UPGRADE DATABASE:
+    alembic upgrade head
+"""
